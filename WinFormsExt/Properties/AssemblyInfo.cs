04e7bb04cce72f2af6553b4a36839ef8bf16ccd9
Adam O'Neil <adamosoftware@gmail.com>
2017-05-07 17:37:10 -0400

    initial check in

--- WinFormsExt/Properties/AssemblyInfo.cs
+++ WinFormsExt/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WinFormsExt")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extension methods for WinForms apps")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Adam O'Neil Software")]
 [assembly: AssemblyProduct("WinFormsExt")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Adam O'Neil Software 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ WinFormsExt/TreeViewExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AdamOneilSoftware
+{
+    public static class TreeViewExtensions
+	{
+		public static List<T> FindNodes<T>(this TreeView treeView) where T : TreeNode
+		{
+			List<T> results = new List<T>();
+			foreach (TreeNode node in treeView.Nodes)
+			{
+				AddResult(node, results);
+				FindNodesR<T>(node, results);
+			}
+			return results;
+		}
+
+		public static List<T> FindNodes<T>(this TreeNode treeNode) where T : TreeNode
+		{
+			List<T> results = new List<T>();
+			FindNodesR<T>(treeNode, results);
+			return results;
+		}
+
+		private static void FindNodesR<T>(TreeNode parent, List<T> results) where T : TreeNode
+		{
+			if (parent == null) return;
+			foreach (TreeNode node in parent.Nodes)
+			{
+				AddResult(node, results);
+				FindNodesR(node, results);
+			}
+		}
+
+		private static void AddResult<T>(TreeNode node, List<T> results) where T : TreeNode
+		{
+			T checkNode = node as T;
+			if (checkNode != null) results.Add(checkNode);
+		}
+	}
+}
