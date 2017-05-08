19a84b485b002d45a5d8b39ee61d3e56b48840aa
Adam O'Neil <adamosoftware@gmail.com>
2017-05-08 11:02:34 -0400

    version 1.0.1, recursive argument

--- WinFormsExt/TreeViewExtensions.cs
+++ WinFormsExt/TreeViewExtensions.cs
@@ -5,31 +5,31 @@ namespace AdamOneilSoftware
 {
     public static class TreeViewExtensions
 	{
-		public static List<T> FindNodes<T>(this TreeView treeView) where T : TreeNode
+		public static List<T> FindNodes<T>(this TreeView treeView, bool recursive = true) where T : TreeNode
 		{
 			List<T> results = new List<T>();
 			foreach (TreeNode node in treeView.Nodes)
 			{
 				AddResult(node, results);
-				FindNodesR<T>(node, results);
+				if (recursive) FindNodesR<T>(node, results);
 			}
 			return results;
 		}
 
-		public static List<T> FindNodes<T>(this TreeNode treeNode) where T : TreeNode
+		public static List<T> FindNodes<T>(this TreeNode treeNode, bool recursive = true) where T : TreeNode
 		{
 			List<T> results = new List<T>();
-			FindNodesR<T>(treeNode, results);
+			FindNodesR<T>(treeNode, results, recursive);
 			return results;
 		}
 
-		private static void FindNodesR<T>(TreeNode parent, List<T> results) where T : TreeNode
+		private static void FindNodesR<T>(TreeNode parent, List<T> results, bool recursive = true) where T : TreeNode
 		{
 			if (parent == null) return;
 			foreach (TreeNode node in parent.Nodes)
 			{
 				AddResult(node, results);
-				FindNodesR(node, results);
+				if (recursive) FindNodesR(node, results);
 			}
 		}
 
