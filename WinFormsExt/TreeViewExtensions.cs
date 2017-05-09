46aae66cdc975f21a4134af5bf6e0ce9bb320717
Adam O'Neil <adamosoftware@gmail.com>
2017-05-09 15:58:30 -0400

    codemaid cleanup

--- WinFormsExt/TreeViewExtensions.cs
+++ WinFormsExt/TreeViewExtensions.cs
@@ -5,44 +5,44 @@ using System.Windows.Forms;
 namespace AdamOneilSoftware
 {
     public static class TreeViewExtensions
-	{
-		public static List<T> FindNodes<T>(this TreeView treeView, bool recursive = true) where T : TreeNode
-		{
-			List<T> results = new List<T>();
-			foreach (TreeNode node in treeView.Nodes)
-			{
-				AddResult(node, results);
-				if (recursive) FindNodesR<T>(node, results);
-			}
-			return results;
-		}
+    {
+        public static List<T> FindNodes<T>(this TreeView treeView, bool recursive = true) where T : TreeNode
+        {
+            List<T> results = new List<T>();
+            foreach (TreeNode node in treeView.Nodes)
+            {
+                AddResult(node, results);
+                if (recursive) FindNodesR<T>(node, results);
+            }
+            return results;
+        }
 
-		public static List<T> FindNodes<T>(this TreeNode treeNode, bool recursive = true) where T : TreeNode
-		{
-			List<T> results = new List<T>();
-			FindNodesR<T>(treeNode, results, recursive);
-			return results;
-		}
+        public static List<T> FindNodes<T>(this TreeNode treeNode, bool recursive = true) where T : TreeNode
+        {
+            List<T> results = new List<T>();
+            FindNodesR<T>(treeNode, results, recursive);
+            return results;
+        }
 
-		private static void FindNodesR<T>(TreeNode parent, List<T> results, bool recursive = true) where T : TreeNode
-		{
-			if (parent == null) return;
-			foreach (TreeNode node in parent.Nodes)
-			{
-				AddResult(node, results);
-				if (recursive) FindNodesR(node, results);
-			}
-		}
+        private static void FindNodesR<T>(TreeNode parent, List<T> results, bool recursive = true) where T : TreeNode
+        {
+            if (parent == null) return;
+            foreach (TreeNode node in parent.Nodes)
+            {
+                AddResult(node, results);
+                if (recursive) FindNodesR(node, results);
+            }
+        }
 
-		private static void AddResult<T>(TreeNode node, List<T> results) where T : TreeNode
-		{
-			T checkNode = node as T;
-			if (checkNode != null) results.Add(checkNode);
-		}
+        private static void AddResult<T>(TreeNode node, List<T> results) where T : TreeNode
+        {
+            T checkNode = node as T;
+            if (checkNode != null) results.Add(checkNode);
+        }
 
         public static void Execute(this TreeView treeView, Action<TreeNode> action, Func<TreeNode, bool> filter = null)
         {
-            foreach (TreeNode node in treeView.Nodes) ExecuteR(node, action, filter);            
+            foreach (TreeNode node in treeView.Nodes) ExecuteR(node, action, filter);
         }
 
         public static void Execute(this TreeNode treeNode, Action<TreeNode> action, Func<TreeNode, bool> filter = null)
@@ -58,5 +58,5 @@ namespace AdamOneilSoftware
                 foreach (TreeNode childNode in node.Nodes) ExecuteR(childNode, action, filter);
             }
         }
-	}
+    }
 }
