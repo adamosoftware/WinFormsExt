4ef619d3c847e5aa1dadcffac5c166b8f42a9253
Adam O'Neil <adamosoftware@gmail.com>
2017-05-17 15:12:33 -0400

    version 1.0.6, filter argument

--- WinFormsExt/TreeViewExtensions.cs
+++ WinFormsExt/TreeViewExtensions.cs
@@ -6,38 +6,38 @@ namespace AdamOneilSoftware
 {
     public static class TreeViewExtensions
     {
-        public static List<T> FindNodes<T>(this TreeView treeView, bool recursive = true) where T : TreeNode
+        public static List<T> FindNodes<T>(this TreeView treeView, bool recursive = true, Func<T, bool> filter = null) where T : TreeNode
         {
             List<T> results = new List<T>();
             foreach (TreeNode node in treeView.Nodes)
             {
                 AddResult(node, results);
-                if (recursive) FindNodesR<T>(node, results);
+                if (recursive) FindNodesR<T>(node, results, true, filter);
             }
             return results;
         }
 
-        public static List<T> FindNodes<T>(this TreeNode treeNode, bool recursive = true) where T : TreeNode
+        public static List<T> FindNodes<T>(this TreeNode treeNode, bool recursive = true, Func<T, bool> filter = null) where T : TreeNode
         {
             List<T> results = new List<T>();
-            FindNodesR<T>(treeNode, results, recursive);
+            FindNodesR<T>(treeNode, results, recursive, filter);
             return results;
         }
 
-        private static void FindNodesR<T>(TreeNode parent, List<T> results, bool recursive = true) where T : TreeNode
+        private static void FindNodesR<T>(TreeNode parent, List<T> results, bool recursive = true, Func<T, bool> filter = null) where T : TreeNode
         {
             if (parent == null) return;
             foreach (TreeNode node in parent.Nodes)
             {
                 AddResult(node, results);
-                if (recursive) FindNodesR(node, results);
+                if (recursive) FindNodesR(node, results, true, filter);
             }
         }
 
-        private static void AddResult<T>(TreeNode node, List<T> results) where T : TreeNode
+        private static void AddResult<T>(TreeNode node, List<T> results, Func<T, bool> filter = null) where T : TreeNode
         {
             T checkNode = node as T;
-            if (checkNode != null) results.Add(checkNode);
+            if (checkNode != null && (filter?.Invoke(checkNode) ?? true)) results.Add(checkNode);
         }
 
         public static void Execute(this TreeView treeView, Action<TreeNode> action, Func<TreeNode, bool> filter = null)
