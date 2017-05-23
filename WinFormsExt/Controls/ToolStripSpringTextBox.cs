4ea694cf4dc23e070c4259c8b214dc570192c67f
Adam O'Neil <adamosoftware@gmail.com>
2017-05-23 11:46:16 -0400

    version 1.0.8 added ToolStripSpringTextBox

---
+++ WinFormsExt/Controls/ToolStripSpringTextBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamOneilSoftware.Controls
+{
+    // thanks to https://msdn.microsoft.com/en-us/library/ms404304(v=vs.110).aspx
+
+    public class ToolStripSpringTextBox : ToolStripTextBox
+    {
+        public override Size GetPreferredSize(Size constrainingSize)
+        {
+            if (IsOnOverflow || Owner.Orientation == Orientation.Vertical)
+            {
+                return DefaultSize;
+            }
+
+            int width = Owner.DisplayRectangle.Width;
+            if (Owner.OverflowButton.Visible)
+            {
+                width = width - Owner.OverflowButton.Width - Owner.OverflowButton.Margin.Horizontal;
+            }
+
+            int springBoxCount = 0;
+            foreach (ToolStripItem item in Owner.Items)
+            {                
+                if (item.IsOnOverflow) continue;
+
+                if (item is ToolStripSpringTextBox)
+                {                    
+                    springBoxCount++;
+                    width -= item.Margin.Horizontal;
+                }
+                else
+                {
+                    width = width - item.Width - item.Margin.Horizontal;
+                }
+            }
+
+            if (springBoxCount > 1) width /= springBoxCount;
+
+            if (width < DefaultSize.Width) width = DefaultSize.Width;
+
+            Size size = base.GetPreferredSize(constrainingSize);
+            size.Width = width;
+            return size;
+        }
+    }
+}
