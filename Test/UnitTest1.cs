4ff9ccf45205c1b3e932d612a980571cde62f9f4
Adam O'Neil <adamosoftware@gmail.com>
2017-06-01 09:52:42 -0400

    version 1.0.10, ViewFolder and ViewDocument methods

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -12,5 +12,17 @@ namespace Test
         {
             Shell.ViewFileLocation(@"C:\Users\Adam\Dropbox\Visual Studio 2017\Projects\PwdKeeper\PwdKeeper.sln");
         }
+
+        [TestMethod]
+        public void ViewFolder()
+        {
+            Shell.ViewFolder(@"C:\Users\Adam\Dropbox\Visual Studio 2017\Projects");
+        }
+
+        [TestMethod]
+        public void ViewDocument()
+        {
+            Shell.ViewDocument(@"C:\Users\Adam\Dropbox\Visual Studio 2017\Projects\Postulate.Sql\Postulate.Sql.sln");
+        }
     }
 }
--- WinFormsExt/Shell.cs
+++ WinFormsExt/Shell.cs
@@ -11,10 +11,24 @@ namespace AdamOneilSoftware
     public static class Shell
     {
         public static void ViewFileLocation(string fileName)
-        {            
+        {
             ProcessStartInfo psi = new ProcessStartInfo("explorer.exe");
             psi.Arguments = $"/select,{fileName}";
             Process.Start(psi);
-        }        
+        }
+        
+        public static void ViewFolder(string folder)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("explorer.exe");
+            psi.Arguments = folder;
+            Process.Start(psi);
+        }
+
+        public static void ViewDocument(string fileName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(fileName);
+            psi.UseShellExecute = true;
+            Process.Start(psi);
+        }
     }
 }
