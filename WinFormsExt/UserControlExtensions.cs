1761cb10d6a909fa93d7382840bf55f5ed6fb496
Adam O'Neil <adamosoftware@gmail.com>
2017-12-08 18:39:45 -0500

    added UserControlExtensions

---
+++ WinFormsExt/UserControlExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace AdamOneilSoftware
+{
+    public static class UserControlExtensions
+    {
+        public static bool IsDesignMode(this UserControl userControl)
+        {
+            return (LicenseManager.UsageMode == LicenseUsageMode.Designtime);
+        }
+    }
+}
