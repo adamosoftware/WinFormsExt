b8fa81c9bf5815e5f404f6b60dff295d358fdd90
Adam O'Neil <adamosoftware@gmail.com>
2017-12-10 16:10:44 -0500

    version 1.0.13, encryption and DataGridView extensions

---
+++ WinFormsExt/DataGridViewExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AdamOneilSoftware
+{
+    public static class DataGridViewExtensions
+    {
+        public static void FillFromEnum<TEnum>(this DataGridViewComboBoxColumn column)
+        {
+            column.DataSource = (from TEnum enumVal in Enum.GetValues(typeof(TEnum))
+                                 select new KeyValuePair<int, string>(Convert.ToInt32(enumVal), enumVal.ToString())).ToList();
+            column.ValueMember = "Key";
+            column.DisplayMember = "Value";
+        }
+    }
+}
---
+++ WinFormsExt/Encryption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdamOneilSoftware
+{
+    public static class Encryption
+    {
+        public static string Encrypt(this string clearText, DataProtectionScope scope = DataProtectionScope.CurrentUser)
+        {
+            byte[] clearBytes = Encoding.ASCII.GetBytes(clearText);
+            byte[] encryptedBytes = ProtectedData.Protect(clearBytes, null, scope);
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public static string Decrypt(this string encryptedText, DataProtectionScope scope = DataProtectionScope.CurrentUser)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+            byte[] clearBytes = ProtectedData.Unprotect(encryptedBytes, null, scope);
+            return Encoding.ASCII.GetString(clearBytes);
+        }
+    }
+}
